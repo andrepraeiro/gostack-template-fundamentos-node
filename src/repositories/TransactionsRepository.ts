3b693359f459c8d335762b67ee135dbcbcc77968
Andre Praeiro <andrepraeiro@gmail.com>
2020-04-16 20:05:53 -0400

    fix all tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+    balance.income = this.transactions.reduce(
+      (totalIncome = 0, currentTransaction) => {
+        return currentTransaction.type === 'income'
+          ? totalIncome + currentTransaction.value
+          : totalIncome;
+      },
+      0,
+    );
+
+    balance.outcome = this.transactions.reduce(
+      (totalOutcome = 0, currentTransaction) => {
+        return currentTransaction.type === 'outcome'
+          ? totalOutcome + currentTransaction.value
+          : totalOutcome;
+      },
+      0,
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome' && this.getBalance().total < value) {
+      throw Error('The value of outcome is greater than the total balance');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
